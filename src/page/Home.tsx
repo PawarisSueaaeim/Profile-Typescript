32523debf23c1d746749d1bb60eaace41b28b5b5
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-01-19 21:44:34 +0700

    home page

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -7,8 +7,9 @@ import '../style/common/pagecss/Home.css';
 
 const Home = () => {
   return (
-    <Container>
-      <Row>
+    <div className="home-bg-dark">
+      <Container>
+        <Row>
         <Col sm={4}>
           <img  className="image-profile" src="https://scontent.fbkk4-3.fna.fbcdn.net/v/t39.30808-6/277466599_3021638301387320_606080316384089201_n.jpg?stp=dst-jpg_s552x414&_nc_cat=100&ccb=1-7&_nc_sid=da31f3&_nc_eui2=AeHyO9R1tHiLEG9ndrbPAOQ7GfYWV9HPceoZ9hZX0c9x6oxZ4jWfRiAQXD02BwGiwB9OZWSIxorGUs253tG9Inmp&_nc_ohc=jN9wVbKvUuwAX8yYJbG&_nc_zt=23&_nc_ht=scontent.fbkk4-3.fna&oh=00_AfCkoeNVx-gp6qs_Z8i4SsWt0VAqBgwGRU_EGRLdYb2uuQ&oe=63C9A0AC"></img>
         </Col>
@@ -17,6 +18,8 @@ const Home = () => {
         </Col>
       </Row>
     </Container>
+    </div>
+   
   );
 };
 
