6eac52c4a2e9ff7b13b6730419fe78330edfe164
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-01-22 23:13:08 +0700

    boooooo

--- src/components/SwitchDarkLightMode.tsx
+++ src/components/SwitchDarkLightMode.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
 interface SwitchDarkLightMode {
-    
+    onClick: (event: Event) => void;
 }
 
 const SwitchDarkLightMode = () => {
-    const [lightMode, setLightmode] = React.useState(false);
+    const [lightMode, setLightmode] = React.useState(true);
 
     const onClick = () => {
       setLightmode(!lightMode);
     };
+
+    return (
+
+    );
 };
 
 export default SwitchDarkLightMode;
